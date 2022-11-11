51cd1028e9953f0930690e088dca4facb06159f7
leehyeona82 <109805685+leehyeona82@users.noreply.github.com>
2022-11-12 01:40:43 +0900

    Feat.leehyunah (#16)
    * feat.메인페이지 중앙정렬[#9]
    
    * 메인페이지 게시물들을 화면 가로 비율에 맞춰서 양 옆 비율이 대칭을 이루도록 함
    Resolves #9
    
    * feat.main Title line
    
    메인타이틀 중앙부터 밑줄효과
    Resolves #12
    
    * feat. 메인메뉴바 포커스 라인
    
    메인메뉴바 포커스라인
    
    #15

--- deblog/src/component/submitMenu/SubmitButton.js
+++ deblog/src/component/submitMenu/SubmitButton.js
@@ -7,12 +7,36 @@ width:100px;
 height:45px;
 outline:none;
 border:none;
-background-color:#EEE8AA;
+background-color:#EFA4A4;
 font-size:20px;
 font-family: 'Noto Serif KR', serif;
 background-color:transparent;
+:before{
+    content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-color: #000;
+  -webkit-transform: scaleY(.3);
+  transform: scaleY(.3);
+  opacity: 0;
+  transition: all .3s
+  }
 :hover{
-    background-color:#EEE8AA; 
+    color:#000000;  
+}
+:hover::before{
+    opacity: 1;
+  background-color: #F8E5A2;
+  -webkit-transform: scaleY(1);
+  transform: scaleY(1);
+  transition: -webkit-transform .6s cubic-bezier(.08, .35, .13, 1.02), opacity .4s;
+  transition: transform .6s cubic-bezier(.08, .35, .13, 1.02), opacity
+} 
 }
 `
 
