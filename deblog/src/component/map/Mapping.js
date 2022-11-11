f28c2707bd1709ab3d079f70539c28e791a05103
sallyhyunakim <109338017+sallyhyunakim@users.noreply.github.com>
2022-11-12 03:38:34 +0900

    feat. mapping 개발 [#18]
    * mapping 개발
    
    Resolves #18

---
+++ deblog/src/component/map/Mapping.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react';
+
+
+
+const Mapping=()=>{
+
+    useEffect(()=>{
+
+        var container = document.getElementById('map');
+        var options = {
+            center: new window.kakao.maps.LatLng(37.54957967842569, 127.07511575149536),
+            level: 3
+        };
+        var map = new window.kakao.maps.Map(container, options);
+    }, [])
+
+    return(
+        <div >
+            <div id="map" style={{width:"300px", height:"300px"}}></div>
+        </div>
+    )
+}
+
+export default Mapping;
--- deblog/src/page/CulturePage.js
+++ deblog/src/page/CulturePage.js
@@ -5,7 +5,7 @@ const CulturePage = () => {
     return (
         <div>
             <MenuBar />
-            현아가 만들 페이지
+            
         </div>
     )
 }
