5304cc48191934625aa91f563e8da7ecfa29b140
KYUTAE PARK <rbxo0109@gmail.com>
2022-11-17 17:11:50 +0900

    Remove server

--- deblog/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import BookReducer from "./Book-slice";
-
-
-const store = configureStore({
-    reducer: { book: BookReducer }
-});
-
-export default store
