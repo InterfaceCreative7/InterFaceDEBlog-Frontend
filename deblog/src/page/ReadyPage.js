0388df9c8f3a6f78564bb3f52fd56c105369fef0
박규태 <79373803+KyuTae98@users.noreply.github.com>
2022-11-29 21:56:05 +0900

    Kyutae (#50)
    * feat. post width 변경
    
    * feat. mapping merge
    
    * mapping 개발
    
    * Feat. main화면 css 수정 [#24]
    
    * main화면 culture 화면 css 수정.
    
    Resolves #24
    
    * feat. 방명록 개발[#26]
    
    * 서버 연동 및 방명록 개발
    
    * Remove server
    
    * feat. server ip gitigmore
    
    * 서버 부분의 ip는 소중하기 때문에 gitignore함
    
    * feat. tag, 삭제
    
    * tag 정리, 삭제 버튼 수정
    
    * feat. about페이지 수정
    
    * about페이지 글 크기 css편집
    
    * feat. post내용 추가 + 방명록 기능
    
    * post 내용을 추가 후 방명록 기능을 추가 했습니다.
    
    * feat. bookpage 수정
    
    * bookpage css 수정중
    
    * feat. 방명록 부분 디테일 수정
    
    * 방명록 부분의 디자인 부분 디테일하게 수정하였습니다.
    
    * feat. 중간결산
    
    * 중간결산입니다
    
    * feat. 페이지네이션 개발 [#39]
    
    * 페이지네이션 개발 (css 개발 필요)
    
    Resolves #39
    
    * Update package.json
    
    * feat, 페이지네이션 수정
    
    * 페이지네이션 수정
    
    * feat. Submit Page수정
    
    * feat. Submit page, ready page 개발
    
    * feat, 게시글 등록
    
    * api update
    
    * feat. 정리본
    
    정리본

--- deblog/src/page/ReadyPage.js
+++ deblog/src/page/ReadyPage.js
@@ -1,16 +1,25 @@
 import classes from "./ReadyPage.module.css"
 import { useSelector } from "react-redux";
-
+import api from "../component/server/Auth.js"
 
 const ReadyPage = () => {
     const body = useSelector(state => state.post.body)
     const title = useSelector(state => state.post.title)
+    const submitBody = () => {
+        api.post("/posts/upload", {
+            title: title,
+            blogBody: body,
+            tag: [],
+            imageUrl: "",
+            writername: "박규태"
+        })
+    }
     return (
         <div className={classes.main}>
             <form className={classes.form}>
                 <div className={`${classes.pt1} ${classes.pt}`}>
                     <input className={classes.input} type="file" />
-                    <button className={classes.btn}>제출</button>
+                    <button onClick={submitBody} className={classes.btn}>제출</button>
                 </div>
                 <div className={`${classes.pt2} ${classes.pt}`}>
                     <input className={classes.input} placeholder="작성자명을 입력해주세요!" />
